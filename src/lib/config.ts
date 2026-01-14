6cf454102558949ad4bc809b8189fe0a6d688468
imlymei <lymeicontato@gmail.com>
2026-01-14 15:10:41 -0300

    feat: add basic utils and theme system

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import packageData from "../../package.json";
+
+export const APP = {
+  name: "OpenHours",
+  version: packageData.version,
+  description: "A simple Open-Source Hours manager",
+};
